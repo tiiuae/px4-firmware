114fbb2e2268e18b45ce2929908749b8b9a861ab
Jukka Laitinen <jukkax@ssrc.tii.ae>
2021-06-04 15:48:56 +0300

    DON'T MERGE: add px4 crypto support for px4_fmu_v5 for testing
    use sw_crypto and stub_keystore
    
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- boards/px4/fmu-v5/default.cmake
+++ boards/px4/fmu-v5/default.cmake
@@ -10,6 +10,8 @@ px4_add_board(
 	IO px4_io-v2_default
 	UAVCAN_INTERFACES 2
 	UAVCAN_TIMER_OVERRIDE 6
+        CRYPTO sw_crypto
+        KEYSTORE stub_keystore
 	SERIAL_PORTS
 		GPS1:/dev/ttyS0
 		TEL1:/dev/ttyS1
