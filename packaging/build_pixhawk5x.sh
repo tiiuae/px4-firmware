3340519d38ce6b39fb2332847af3f6e4ad88c993
Jari Nippula <jarix@ssrc.tii.ae>
2022-09-13 09:07:30 +0300

    Drop obsolete fmu-v5 (pixhawk4) building

--- packaging/build_pixhawk5x.sh
+++ packaging/build_pixhawk5x.sh
@@ -5,8 +5,7 @@ source /opt/ros/galactic/setup.sh
 export SIGNING_TOOL=Tools/cryptotools.py
 
 # Remove old build output
-rm -Rf build/px4_fmu-v5_ssrc build/px4_fmu-v5x_ssrc
+rm -Rf build/px4_fmu-v5x_ssrc
 
 # Build
-make px4_fmu-v5_ssrc
 make px4_fmu-v5x_ssrc
