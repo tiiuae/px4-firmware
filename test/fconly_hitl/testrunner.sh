afcfdb293d0010c1dd3b3121781ee71fe3fd8dce
Matias Soini <matias.soini@unikie.com>
2022-11-25 08:50:20 +0200

    WIP updated hitl_mission.py and testrunner.sh

--- test/fconly_hitl/testrunner.sh
+++ test/fconly_hitl/testrunner.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-original_dir = "${0%/*}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+smart_plug_ip="172.18.32.109"
 
 cd /home/tii/hitl/
 ./remove_server.sh
 sleep 10
 ./start_server.sh &
+# TODO: how to determine that gzserver is started and status is OK?
+sleep 30
 
-#cd "${original_dir}"
+echo "${SCRIPT_DIR}"
+cd "${SCRIPT_DIR}/../../Tools"
+pwd
 
-python3 hitl_mission.py
+# Flash saluki with FW from the build
+python3 px_uploader.py --port /dev/ttyUSB0 --baud-bootloader 2000000 /home/tii/Downloads/ssrc_saluki-v1_default-1.13.0-alpha1-5019-g38a6a4a9cd.px4 &
+		
+# Reboot saluki
+kasa --host ${smart_plug_ip} off
+sleep 2
+kasa --host ${smart_plug_ip} on
+
+sleep 180
+
+cd /home/tii/hitl/
+
+#python3 hitl_mission.py
+
+
