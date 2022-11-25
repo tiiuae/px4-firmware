b880064788a808af6aceb6dea5ffde753c054ccf
Matias Soini <matias.soini@unikie.com>
2022-11-25 11:21:42 +0200

    WIP integrated HITL tests into build workflow

--- test/fconly_hitl/testrunner.sh
+++ test/fconly_hitl/testrunner.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+target_px4_firmware="${1}"
 
 smart_plug_ip="172.18.32.109"
 
@@ -17,7 +18,7 @@ cd "${SCRIPT_DIR}/../../Tools"
 pwd
 
 # Flash saluki with FW from the build
-python3 px_uploader.py --port /dev/ttyUSB0 --baud-bootloader 2000000 /home/tii/Downloads/ssrc_saluki-v1_default-1.13.0-alpha1-5019-g38a6a4a9cd.px4 &
+python3 px_uploader.py --port /dev/ttyUSB0 --baud-bootloader 2000000 bin/${target_px4_firmware} &
 		
 # Reboot saluki
 kasa --host ${smart_plug_ip} off
