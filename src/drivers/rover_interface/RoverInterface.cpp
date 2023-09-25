95af0b1b7d0d846dcb853e3a1375b11057191458
haitomatic <hai.to@unikie.com>
2023-09-25 08:12:05 +0000

    rover interface: allow configurable maximum mission speed

--- src/drivers/rover_interface/RoverInterface.cpp
+++ src/drivers/rover_interface/RoverInterface.cpp
@@ -6,12 +6,14 @@ RoverInterface *RoverInterface::_instance;
 // CAN interface | default is can0
 const char *const RoverInterface::CAN_IFACE = "can0";
 
-RoverInterface::RoverInterface(uint8_t rover_type, uint32_t bitrate, float manual_throttle_max)
+RoverInterface::RoverInterface(uint8_t rover_type, uint32_t bitrate, float manual_throttle_max,
+			       float mission_throttle_max)
 	: ModuleParams(nullptr),
 	  ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::rover_interface),
 	  _rover_type(rover_type),
 	  _bitrate(bitrate),
-	  _manual_throttle_max(manual_throttle_max)
+	  _manual_throttle_max(manual_throttle_max),
+	  _mission_throttle_max(mission_throttle_max)
 {
 	pthread_mutex_init(&_node_mutex, nullptr);
 }
@@ -43,14 +45,14 @@ RoverInterface::~RoverInterface()
 }
 
 
-int RoverInterface::start(uint8_t rover_type, uint32_t bitrate, float manual_throttle_max)
+int RoverInterface::start(uint8_t rover_type, uint32_t bitrate, float manual_throttle_max, float mission_throttle_max)
 {
 	if (_instance != nullptr) {
 		PX4_ERR("Already started");
 		return -1;
 	}
 
-	_instance = new RoverInterface(rover_type, bitrate, manual_throttle_max);
+	_instance = new RoverInterface(rover_type, bitrate, manual_throttle_max, mission_throttle_max);
 
 	if (_instance == nullptr) {
 		PX4_ERR("Failed to allocate RoverInterface object");
@@ -223,7 +225,7 @@ void RoverInterface::ActuatorControlsUpdate()
 		actuator_controls_s actuator_controls_msg;
 
 		if (_actuator_controls_sub.copy(&actuator_controls_msg)) {
-			auto throttle = (_is_manual_mode ? _manual_throttle_max : 1.0f) *
+			auto throttle = (_is_manual_mode ? _manual_throttle_max : _mission_throttle_max) *
 					actuator_controls_msg.control[actuator_controls_s::INDEX_THROTTLE];
 			auto steering = actuator_controls_msg.control[actuator_controls_s::INDEX_YAW];
 			_scout->SetMotionCommand(throttle, steering);
@@ -384,12 +386,18 @@ extern "C" __EXPORT int rover_interface_main(int argc, char *argv[])
 		float manual_throttle_max = 1.0;
 		param_get(param_find("RI_MAN_THR_MAX"), &manual_throttle_max);
 
+		// Mission control mode max throttle (1m/s to 3m/s)
+		float mission_throttle_max = 1.0;
+		param_get(param_find("RI_MIS_THR_MAX"), &mission_throttle_max);
+
 		// Start
 		PX4_INFO("Start Rover Interface to rover type %d at CAN iface %s with bitrate %d bit/s",
 			 rover_type, RoverInterface::CAN_IFACE, can_bitrate);
 		return RoverInterface::start(static_cast<uint8_t>(rover_type),
 					     can_bitrate,
-					     manual_throttle_max);
+					     manual_throttle_max,
+					     mission_throttle_max
+					    );
 	}
 
 	/* commands below assume that the app has been already started */
