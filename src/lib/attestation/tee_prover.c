58f42582fef40963a02837eb775f42a16f4b396a
George Poulios <gpoulios@census-labs.com>
2025-01-24 10:17:08 +0200

    Complete SysSvc proxying to the TEE
    - lib/attestation:
      - new libkatt (INTERFACE) target for e-call definitions
    - boards/ssrc/common:
      - mpfs_dsn.o plucked out of libkarch.a using ar and proxied to TEE
      - tee.h provides now tee_{new_call,call_n_ret} macros
      - No more need for PMP allow entry for SCBCTRL
      - Link against new libkatt to get e-call definitions
    - drivers/pfsoc_keystore:
      - update according to refactorings (same functionality)
    - modules/attestation_agent:
      - update README accordingly
    
    Signed-off-by: George Poulios <gpoulios@census-labs.com>

--- src/lib/attestation/tee_prover.c
+++ src/lib/attestation/tee_prover.c
@@ -92,26 +92,6 @@ void att_free(void *ptr)
  * TEE-based prover implementation
  */
 
-// *INDENT-OFF*
-#define NEW_CALL(fid) { \
-		.funcid = (fid), \
-		.out = 0, \
-		.a[0] = {.v = 0, .size = 0}, \
-		.a[1] = {.v = 0, .size = 0}, \
-		.a[2] = {.v = 0, .size = 0}, \
-		.a[3] = {.v = 0, .size = 0}, \
-		.a[4] = {.v = 0, .size = 0}, \
-		.a[5] = {.v = 0, .size = 0} \
-	}
-// *INDENT-ON*
-
-#define INVOKE(args) \
-	int ret = tee_call(&(args)); \
-	if (ret != 0) { \
-		return ret; \
-	} \
-	return (int)(args).out;
-
 int prover_init(void)
 {
 	/* First, let's set up a TEE-suitable heap */
@@ -127,14 +107,14 @@ int prover_init(void)
 
 	/* Invoke the actual prover_init() now */
 
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_INIT);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_INIT);
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 void prover_destroy(void)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_DESTROY);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_DESTROY);
 
 	tee_call(&args);
 
@@ -149,27 +129,27 @@ void prover_destroy(void)
 
 int prover_session_state(uint32_t session_id)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_SESSION_STATE);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_SESSION_STATE);
 
 	args.a[0].v = session_id;
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 int prover_session_init(uint32_t session_id, int server)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_SESSION_INIT);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_SESSION_INIT);
 
 	args.a[0].v = session_id;
 
 	args.a[1].v = (uintptr_t)server;
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 void prover_session_close(uint32_t session_id, uint8_t *out, size_t *out_len)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_SESSION_CLOSE);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_SESSION_CLOSE);
 
 	args.a[0].v = session_id;
 
@@ -186,7 +166,7 @@ int prover_session_recv(uint32_t session_id, const uint8_t *in, size_t *in_len,
 			uint8_t *out, size_t *out_len, uint8_t *appdata,
 			size_t appdata_len)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_SESSION_RECV);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_SESSION_RECV);
 
 	args.a[0].v = session_id;
 
@@ -205,13 +185,13 @@ int prover_session_recv(uint32_t session_id, const uint8_t *in, size_t *in_len,
 	args.a[5].p = appdata;
 	args.a[5].size = appdata_len;
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 int prover_session_send(uint32_t session_id, const uint8_t *appdata,
 			size_t appdata_len, uint8_t *out, size_t *out_len)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_SESSION_SEND);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_SESSION_SEND);
 
 	args.a[0].v = session_id;
 
@@ -224,13 +204,13 @@ int prover_session_send(uint32_t session_id, const uint8_t *appdata,
 	args.a[3].p = out_len;
 	args.a[3].size = sizeof(size_t);
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 int prover_challenge(const uint8_t *tlv_challenge, size_t chlen,
 		     uint8_t *tlv_report, size_t relen)
 {
-	teeioc_call_t args = NEW_CALL(SBI_EXT_PRV_CHALLENGE);
+	teeioc_call_t args = tee_new_call(SBI_EXT_PRV_CHALLENGE);
 
 	args.a[0].p = (uint8_t *)tlv_challenge;
 	args.a[0].size = chlen;
@@ -238,7 +218,7 @@ int prover_challenge(const uint8_t *tlv_challenge, size_t chlen,
 	args.a[1].p = tlv_report;
 	args.a[1].size = relen;
 
-	INVOKE(args);
+	tee_call_n_ret(&args);
 }
 
 
