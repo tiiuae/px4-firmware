2ceb605959c2d95d4948b844575d37b1de6dde59
Jari Nippula <jari.nippula@tii.ae>
2025-04-24 16:40:47 +0300

    Commander: Critical Action req/rel

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -533,6 +533,16 @@ static constexpr const char *arm_disarm_reason_str(arm_disarm_reason_t calling_r
 
 transition_result_t Commander::arm(arm_disarm_reason_t calling_reason, bool run_preflight_checks)
 {
+
+	if (!_crit_action.request(ACTION_COMMANDER_COMP_ID) && run_preflight_checks) {
+		mavlink_log_critical(&_mavlink_log_pub, "Arming denied: critical activity blocked\t");
+		events::send(events::ID("commander_arm_denied_critical_activity_blocked"),
+		{events::Log::Critical, events::LogInternal::Info},
+		"Arming denied: system critical activity blocked");
+		tune_negative(true);
+		return TRANSITION_DENIED;
+	}
+
 	// allow a grace period for re-arming: preflight checks don't need to pass during that time, for example for accidential in-air disarming
 	if (calling_reason == arm_disarm_reason_t::rc_switch
 	    && (hrt_elapsed_time(&_last_disarmed_timestamp) < 5_s)) {
@@ -548,6 +558,7 @@ transition_result_t Commander::arm(arm_disarm_reason_t calling_reason, bool run_
 				{events::Log::Critical, events::LogInternal::Info},
 				"Arming denied: throttle above center");
 				tune_negative(true);
+				_crit_action.release(ACTION_COMMANDER_COMP_ID);
 				return TRANSITION_DENIED;
 			}
 
@@ -559,6 +570,7 @@ transition_result_t Commander::arm(arm_disarm_reason_t calling_reason, bool run_
 				{events::Log::Critical, events::LogInternal::Info},
 				"Arming denied: high throttle");
 				tune_negative(true);
+				_crit_action.release(ACTION_COMMANDER_COMP_ID);
 				return TRANSITION_DENIED;
 			}
 
@@ -570,6 +582,7 @@ transition_result_t Commander::arm(arm_disarm_reason_t calling_reason, bool run_
 			{events::Log::Critical, events::LogInternal::Info},
 			"Arming denied: switch to manual mode first");
 			tune_negative(true);
+			_crit_action.release(ACTION_COMMANDER_COMP_ID);
 			return TRANSITION_DENIED;
 		}
 	}
@@ -587,6 +600,7 @@ transition_result_t Commander::arm(arm_disarm_reason_t calling_reason, bool run_
 
 	} else if (arming_res == TRANSITION_DENIED) {
 		tune_negative(true);
+		_crit_action.release(ACTION_COMMANDER_COMP_ID);
 	}
 
 	return arming_res;
@@ -634,6 +648,8 @@ transition_result_t Commander::disarm(arm_disarm_reason_t calling_reason, bool f
 	} else if (arming_res == TRANSITION_DENIED) {
 		tune_negative(true);
 	}
+
+	_crit_action.release(ACTION_COMMANDER_COMP_ID);
 
 	return arming_res;
 }
