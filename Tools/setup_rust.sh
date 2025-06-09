dffe1aeb0dab6a2667b16a60ff1e8c4e380d6f9e
Jari Nippula <jari.nippula@tii.ae>
2025-06-09 11:37:06 +0300

    Add Tools/setup_rust.sh

---
+++ Tools/setup_rust.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Pre-requirements
+# apt update && apt install curl build-essential
+
+# Remove old versions
+if dpkg -l | grep -q bindgen; then
+    sudo dpkg --remove bindgen
+fi
+if dpkg -l | grep -q cargo; then
+    sudo dpkg --remove cargo
+fi
+if dpkg -l | grep -q rustc; then
+    sudo dpkg --remove rustc
+fi
+
+
+# Install Rust compiler
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Source the cargo environment to update PATH
+source "$HOME/.cargo/env"
+
+rustup toolchain install stable-x86_64-unknown-linux-gnu
+rustup target add --toolchain stable-x86_64-unknown-linux-gnu aarch64-unknown-none
+
+rustup toolchain install 1.81.0-x86_64-unknown-linux-gnu
+rustup target add --toolchain 1.81.0-x86_64-unknown-linux-gnu riscv64gc-unknown-none-elf
+cargo install --force bindgen-cli@0.70.1
