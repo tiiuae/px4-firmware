2f0acb31cbdf6b694a0541ed77731ab4735ec985
Jani Paalijarvi <jani.paalijarvi@unikie.com>
2025-11-26 14:29:47 +0200

    Use secp384r1 key1 as default for saluki-nxp93
    MPFS based salukis are using ed25519
    
    Update saluki-sec-scripts and saluki-nxp93 submodules
    
    Signed-off-by: Jani Paalijarvi <jani.paalijarvi@unikie.com>

--- packaging/build_px4fw.sh
+++ packaging/build_px4fw.sh
@@ -22,7 +22,11 @@ else
         # use the PX4 default test key.
         if [[ $NAME = saluki* ]]
         then
-            default_signing_args=Tools/saluki-sec-scripts/test_keys/$NAME/ed25519_test_key.pem
+            if [[ "$NAME" == "saluki-nxp93" ]]; then
+                default_signing_args=Tools/saluki-sec-scripts/test_keys/$NAME/secp384r1_test_key1.pem
+            else
+                default_signing_args=Tools/saluki-sec-scripts/test_keys/$NAME/ed25519_test_key.pem
+            fi
 
             if [[ "$SIGNING_KEY" == *hsm* ]]; then
                 echo "Using HSM for signing"
