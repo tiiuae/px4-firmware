fb974291f3ceab8a82d397b1a3b3c8d73907b5cb
RenzoBruzzoneC <renzo.bruzzone@tii.ae>
2026-01-19 12:54:51 +0000

    feat: RTA-XXX monitoring dummy use case with two subscribers

--- src/modules/ztss_monitor/ztss_cases/ztss_case_dummy/ztss_case_dummy.cpp
+++ src/modules/ztss_monitor/ztss_cases/ztss_case_dummy/ztss_case_dummy.cpp
@@ -1,36 +1,66 @@
 #include "ztss_case_dummy.hpp"
 
-
-ZtssCaseDummy::ZtssCaseDummy(ModuleParams *parent): ModuleParams(parent)
+template<int NUMBER_OF_INPUTS>
+ZtssCaseDummy<NUMBER_OF_INPUTS>::ZtssCaseDummy(ModuleParams *parent): ModuleParams(parent)
 {
-	_ztss_use_case_dummy_pub.advertise();
+	// _ztss_use_case_dummy_pub.advertise();
+
+	subscriptions_ = std::array<uORB::Subscription, NUMBER_OF_INPUTS>{
+		uORB::Subscription{ORB_ID(ztss_dummy_trigger_a)},
+		uORB::Subscription{ORB_ID(ztss_dummy_trigger_b)}
+	};
 }
 
-
-void ZtssCaseDummy::update_subscribed_values()
+template<int NUMBER_OF_INPUTS>
+void ZtssCaseDummy<NUMBER_OF_INPUTS>::update_subscribed_values()
 {
-	// TODO (renzo) loop for the array of subcribers.
-	if(_sitl_trigger_of_dummy_safe_actions_sub.updated())
+	for(size_t i =0; i<NUMBER_OF_INPUTS; i++)
 	{
-		PX4_INFO("New data on Dummy case input subcriber");
-		this->updated_subcribers_[0] = true;
-
-	}
-	else
-	{
-		this->updated_subcribers_[0] = false;
+		if (subscriptions_[i].updated())
+		{
+			updated_subcribers_[i] = true;
+			subscriptions_[i].copy(&messages_[i]);
+			continue;
+		}
+		updated_subcribers_[i]=false;
 	}
 }
 
-void ZtssCaseDummy::execute_use_case_safety_evaluation()
+template<int NUMBER_OF_INPUTS>
+void ZtssCaseDummy<NUMBER_OF_INPUTS>::execute_use_case_safety_evaluation()
 {
+	bool all_subcribers_updated = false;
 
-	this->use_case_output_.timestamp = hrt_absolute_time();
-	this->use_case_output_.use_case_status = ztss_monitor_use_case_output_s::INVALID_EXECUTION;
+	for(size_t i =0; i < NUMBER_OF_INPUTS; i++)
+	{
+		all_subcribers_updated = all_subcribers_updated && updated_subcribers_[i];
+	}
 
+	if (all_subcribers_updated)
+	{
+		bool use_case_valid_operation = false;
+		for(size_t i =0; i < NUMBER_OF_INPUTS; i++)
+		{
+			use_case_valid_operation = use_case_valid_operation && messages_[i].request_type == ztss_monitor_use_case_output_s::VALID_EXECUTION;
+		}
+
+		this->use_case_output_.timestamp = hrt_absolute_time();
+		this->use_case_output_.use_case_status = ztss_monitor_use_case_output_s::VALID_EXECUTION;
+
+
+		if (!use_case_valid_operation)
+		{
+			this->use_case_output_.use_case_status = ztss_monitor_use_case_output_s::INVALID_EXECUTION;
+		}
+	}
 }
 
-void ZtssCaseDummy::publish_use_case_status()
+
+template<int NUMBER_OF_INPUTS>
+void ZtssCaseDummy<NUMBER_OF_INPUTS>::publish_use_case_status()
 {
 	this->_ztss_use_case_dummy_pub.publish(this->use_case_output_);
 }
+
+
+template class ZtssCaseDummy<2>;
--- src/modules/ztss_monitor/ztss_cases/ztss_case_dummy/ztss_case_dummy.hpp
+++ src/modules/ztss_monitor/ztss_cases/ztss_case_dummy/ztss_case_dummy.hpp
@@ -5,9 +5,10 @@
 #include <px4_platform_common/module_params.h>
 #include <uORB/topics/ztss_monitor_use_case_output.h>
 #include <array>
+#include <any>
 
 
-
+template<int NUMBER_OF_INPUTS>
 class ZtssCaseDummy : public ModuleParams
 {
 
@@ -23,15 +24,14 @@ void execute_use_case_safety_evaluation();
 void publish_use_case_status();
 
 private:
-// TODO(renzo) make an array of subcriptions where each subscription has a idx.
-// make a structure that take a number of templated subcription
-uORB::Subscription _sitl_trigger_of_dummy_safe_actions_sub{ORB_ID(ztss_dummy_trigger)};
+
+// Publications
 uORB::Publication<ztss_monitor_use_case_output_s> _ztss_use_case_dummy_pub{ORB_ID(ztss_use_case_dummy)};
 
 private:
-std::array<bool, 1> updated_subcribers_;
-
+std::array<bool, NUMBER_OF_INPUTS> updated_subcribers_;
+std::array<uORB::Subscription, NUMBER_OF_INPUTS> subscriptions_;
+std::array<dataman_request_s, NUMBER_OF_INPUTS> messages_;
 ztss_monitor_use_case_output_s use_case_output_;
-
 
 };
