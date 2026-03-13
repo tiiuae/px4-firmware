273c146c77a068add845fc8e5d7bda605682ef99
Jukka Laitinen <jukka.laitinen@tii.ae>
2026-03-13 14:06:34 +0200

    Revert "Add redundancy support to the uavcan esc driver"
    This reverts commit 81b394a3e81e982cd7d5bdde012011ba274c4e90.

--- src/drivers/uavcan/actuators/esc.hpp
+++ src/drivers/uavcan/actuators/esc.hpp
@@ -49,20 +49,11 @@
 #include <uavcan/equipment/esc/Status.hpp>
 #include <lib/perf/perf_counter.h>
 #include <uORB/PublicationMulti.hpp>
-#include <uORB/Subscription.hpp>
 #include <uORB/topics/actuator_outputs.h>
-#include <uORB/topics/actuator_armed.h>
 #include <uORB/topics/esc_status.h>
 #include <drivers/drv_hrt.h>
 #include <lib/mixer_module/mixer_module.hpp>
 #include <parameters/param.h>
-
-#ifdef CONFIG_MODULES_REDUNDANCY
-#include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/redundancy_status.h>
-
-static constexpr int MaxNFCs = vehicle_status_s::MAX_REDUNDANT_CONTROLLERS;
-#endif
 
 class UavcanEscController
 {
@@ -74,7 +65,7 @@ public:
 
 
 	UavcanEscController(uavcan::INode &node);
-	~UavcanEscController();
+	~UavcanEscController() = default;
 
 	int init();
 
@@ -102,8 +93,6 @@ private:
 	 */
 	uint8_t check_escs_status();
 
-	void deleteSubscriptions();
-
 	typedef uavcan::MethodBinder<UavcanEscController *,
 		void (UavcanEscController::*)(const uavcan::ReceivedDataStructure<uavcan::equipment::esc::Status>&)> StatusCbBinder;
 
@@ -129,13 +118,4 @@ private:
 	uavcan::Subscriber<uavcan::equipment::esc::Status, StatusCbBinder>	_uavcan_sub_status;
 
 	param_t _param_handles[MAX_ACTUATORS] {PARAM_INVALID};
-
-#ifdef CONFIG_MODULES_REDUNDANCY
-	uORB::Subscription _actuator_armed_sub {ORB_ID(actuator_armed)};
-	uORB::Subscription _redundancy_status_sub {ORB_ID(redundancy_status)};
-
-	uORB::Subscription *_redundant_actuator_outputs_sub[MaxNFCs] = {nullptr, nullptr};
-
-	bool _redundant_actuator_control_enabled{false};
-#endif
 };
